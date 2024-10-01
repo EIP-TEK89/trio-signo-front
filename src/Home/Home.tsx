29ce02bc8248420c5591f61fb6e1d57792a57f53
Enzo <enzo.pfeiffer@epitech.eu>
2024-10-01 14:59:58 +0200

    feat(Home page) css + graphique template

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -56,12 +56,13 @@ const Home: React.FC = () => {
             <h3 className="section-subtitle">To make sign language learning accessible</h3>
             <p>
               Our platform is designed to break down the barriers to learning sign language,
-              making it accessible to everyone, regardless of their background or location.
+              making it accessible to everyone.
               Whether you're a beginner or someone looking to improve, our intuitive interface
               allows you to start learning anytime, anywhere.
             </p>
           </div>
           <div className="section-image">
+            {/* https://dico.elix-lsf.fr/dictionnaire/accessible */}
             <img src={Acces} alt="Accessibility" />
           </div>
         </div>
@@ -71,11 +72,12 @@ const Home: React.FC = () => {
             <h3 className="section-subtitle">Fun</h3>
             <p>
               Learning a new language doesn't have to be tedious. With engaging exercises, gamification
-              features such as streaks, levels, and leaderboards, we make the learning process exciting
+              , we make the learning process exciting
               and enjoyable. Challenge yourself, track your progress, and compete with others while mastering sign language.
             </p>
           </div>
           <div className="section-image">
+            {/* https://dico.elix-lsf.fr/dictionnaire/amusant */}
             <img src={Fun} alt="Fun" />
           </div>
         </div>
@@ -84,12 +86,13 @@ const Home: React.FC = () => {
           <div className="section-text">
             <h3 className="section-subtitle">Free</h3>
             <p>
-              At TRIO-SIGNO, we believe that learning sign language should be free for everyone. That's why
+              At Signify, we believe that learning sign language should be free for everyone. That's why
               all our resources, lessons, and tools are provided at no cost, ensuring that financial barriers
-              do not prevent anyone from gaining access to vital communication skills.
+              do not prevent anyone from gaining access to a new communication skills.
             </p>
           </div>
           <div className="section-image">
+            {/* https://dico.elix-lsf.fr/dictionnaire/gratuit */}
             <img src={Free} alt="Free" />
           </div>
         </div>
@@ -99,30 +102,23 @@ const Home: React.FC = () => {
             <h3 className="section-subtitle">Assisted by AI</h3>
             <p>
               Our cutting-edge AI technology is here to help you along your learning journey. The AI can recognize
-              your signs, provide instant feedback, and adapt the lessons based on your progress, ensuring a personalized
-              and efficient learning experience. With AI assistance, you'll improve your sign language skills faster and more accurately.
+              your signs, provide instant feedback, ensuring a personalized and efficient learning experience.
+              With AI assistance, you'll improve your sign language skills faster and more accurately.
             </p>
           </div>
           <div className="section-image">
+            {/* faire une animation avec la lettre IA https://dico.elix-lsf.fr/dictionnaire/IA */}
             <img src={IA} alt="AI" />
           </div>
         </div>
       </section>
 
       <section className="team-section">
-        <h2>Meet the Team</h2>
+        <h2 className="section-title">Meet the Team</h2>
         <div className="team">
           <div className="team-member">
             <img src={Lenny} alt="Lenny Vigeon" className="team-photo" />
             <p>Lenny Vigeon - IA Developer</p>
-          </div>
-          <div className="team-member">
-            <img src={Jp} alt="Jean-Pierre Janopoulos" className="team-photo" />
-            <p>Jean-Pierre Janopoulos - IA Developer</p>
-          </div>
-          <div className="team-member">
-            <img src={Valentin} alt="Valentin Maurel" className="team-photo" />
-            <p>Valentin Maurel - Back-end Developer</p>
           </div>
           <div className="team-member">
             <img src={Antoine} alt="Antoine Rospars" className="team-photo" />
@@ -133,6 +129,14 @@ const Home: React.FC = () => {
             <p>Yann Lebib - Front-end Developer</p>
           </div>
           <div className="team-member">
+            <img src={Jp} alt="Jean-Pierre Janopoulos" className="team-photo" />
+            <p>Jean-Pierre Janopoulos - IA Developer</p>
+          </div>
+          <div className="team-member">
+            <img src={Valentin} alt="Valentin Maurel" className="team-photo" />
+            <p>Valentin Maurel - Back-end Developer</p>
+          </div>
+          <div className="team-member">
             <img src={Enzo} alt="Enzo Pfeiffer" className="team-photo" />
             <p>Enzo Pfeiffer - Front-end Developer</p>
           </div>
@@ -140,22 +144,8 @@ const Home: React.FC = () => {
       </section>
 
       <section className="live-demo">
-        <h2>Demo: Webcam continuous hands landmarks detection</h2>
-        <p>Hold your hand in front of your webcam to get real-time hand landmarker detection.Click enable webcam below and grant access to the webcam if prompted.</p>
-
-        <div id="liveView" className="videoView">
-          <button id="webcamButton" className="pushable" onClick={Courses}>
-            <span className="front">
-              Enable WebCam
-            </span>
-          </button>
-          <div>
-            <video id="webcam"></video>
-            <canvas className="output_canvas" id="output_canvas"></canvas>
-          </div>
-        </div>
-
-        <p>Try a lesson now</p>
+        {/* Direct hand-tracking but failed https://codepen.io/mediapipe-preview/pen/gOKBGPN */}
+        <h2 className="section-title">Try a lesson now</h2>
         <button className="pushable" onClick={Courses}>
           <span className="front">
             TRY
@@ -165,47 +155,51 @@ const Home: React.FC = () => {
       </section>
 
       <footer className="footer">
-        <div>About</div>
-        <ul>
-          <li><a href="">Courses</a></li>
-          <li><a href="">Missions</a></li>
-        </ul>
-        <div>Product</div>
-        <ul>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Twitter</a></li>
-          <li><a href="">Youtube</a></li>
-        </ul>
-        <div>Our app</div>
-        <ul>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Twitter</a></li>
-          <li><a href="">Youtube</a></li>
-        </ul>
-        <div>Support</div>
-        <ul>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Twitter</a></li>
-          <li><a href="">Youtube</a></li>
-        </ul>
-        <div>Conditions</div>
-        <ul>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Twitter</a></li>
-          <li><a href="">Youtube</a></li>
-        </ul>
-        <div>Social</div>
-        <ul>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Twitter</a></li>
-          <li><a href="">Youtube</a></li>
-        </ul>
+        <div className="footer-section">
+          <div className='test'>About</div>
+          <ul>
+            <li><a href="">Courses</a></li>
+            <li><a href="">Missions</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <div>Product</div>
+          <ul>
+            <li><a href="">Signify</a></li>
+            <li><a href="">Super Signify</a></li>
+            <li><a href="">Offers Super Signify</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <div>Our app</div>
+          <ul>
+            <li><a href="">Signify for Android</a></li>
+            <li><a href="">Signify for IOS</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <div>Conditions</div>
+          <ul>
+            <li><a href="">Community guidelines</a></li>
+            <li><a href="">Terms</a></li>
+            <li><a href="">Privacy</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <div>Social</div>
+          <ul>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Twitter</a></li>
+            <li><a href="">Youtube</a></li>
+          </ul>
+        </div>
       </footer>
+
     </div>
   );
 };
