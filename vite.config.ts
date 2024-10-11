e2b844a8e7565d035c6782852ec75071e3282c6d
Yann Lebib <yann.lebib@epitech.eu>
2024-10-11 17:31:07 +0200

    build(vite): Front build passed from cra to vite (#23)
    * build(vite): Front build passed from cra to vite
    
    Previously the project was builded with create-react-app but is now builded with vite.
    
    During the developpement i have seen that the node_modules module was not indicated in the .gitignore file causing the push of multiples library files that should normaly be installed by the developpers independently with the command npm install. To correct that mistake i have deleted the node_module folder in this commit and added node_modules in the .gitignore file.
    
    * style: unwanted folder deleted
    
    Deletion of the folder node_modules which shouldn't have been pushed

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+  ],
+  server: {
+    open: true, // automatically open the app in the browser
+    port: 3000,
+  },
+    resolve: {
+        alias: {}
+        ,
+    },
+  build: {
+    outDir: 'build',
+  },
+});
