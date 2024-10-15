38114af267113ef34337df466e6be251ea3172ef
Enzo <enzo.pfeiffer@epitech.eu>
2024-10-15 16:17:37 +0200

    feat(Home) home page done and linked

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginSignup from './LoginSignup/LoginSignup';
 import Home from './Home/Home';
 import Courses from './Courses/Courses';
+import CoursesJourneyHome from './CoursesJourney/Home/CoursesJourneyHome';
+import CoursesJourneyCompetition from './CoursesJourney/Competition/CoursesJourneyCompetition';
+import CoursesJourneyProfile from './CoursesJourney/Profile/CoursesJourneyProfile';
+import CoursesJourneyTraining from './CoursesJourney/Training/CoursesJourneyTraining';
+import CoursesJourneyQuests from './CoursesJourney/Quests/CoursesJourneyQuests';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +16,11 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginSignup />} />
         <Route path="/courses" element={<Courses />} />
+        <Route path="/coursesJourney/home" element={<CoursesJourneyHome />} />
+        <Route path="/coursesJourney/profile" element={<CoursesJourneyProfile />} />
+        <Route path="/coursesJourney/competition" element={<CoursesJourneyCompetition />} />
+        <Route path="/coursesJourney/training" element={<CoursesJourneyTraining />} />
+        <Route path="/coursesJourney/quests" element={<CoursesJourneyQuests />} />
       </Routes>
     </Router>
   );
---
+++ src/CoursesJourney/Quests/CoursesJourneyQuests.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NavBar from '../navBar/navBar';
+
+import './CoursesJourneyQuests.css';
+
+const CoursesJourneyQuests: React.FC = () => {
+
+    return (
+        <div className="">
+            <NavBar />
+        </div>
+    );
+};
+
+export default CoursesJourneyQuests;
