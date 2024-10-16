e4584e92cf45a41913a6c27876be8dc9abb627ea
Enzo <enzo.pfeiffer@epitech.eu>
2024-10-16 12:11:52 +0200

    feat(CoursesJourney) navbar and courses journey added

--- src/CoursesJourney/navBar/navBar.tsx
+++ src/CoursesJourney/navBar/navBar.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
-import { FaHome, FaUsers, FaTrophy, FaBook, FaParachuteBox } from 'react-icons/fa';
 import { useLocation, NavLink } from 'react-router-dom';
 
 import './navBar.css';
+
+import Home from '../../Assets/navBar/home.png';
+import Haltere from '../../Assets/navBar/haltere.png';
+import Trophy from '../../Assets/navBar/trophy.png';
+import Genie from '../../Assets/navBar/genie.png';
+import Chest from '../../Assets/navBar/chest.png';
 
 const NavBar: React.FC = () => {
     const location = useLocation();
@@ -16,53 +21,36 @@ const NavBar: React.FC = () => {
                     to={`${basePath}/home`}
                     className={({ isActive }) => isActive ? "navButton active" : "navButton"}
                 >
-                    <FaHome className='icon' />
+                    <img src={Home} alt="home icon" className="icon" />
                 </NavLink>
 
                 <NavLink
                     to={`${basePath}/training`}
                     className={({ isActive }) => isActive ? "navButton active" : "navButton"}
                 >
-                    <FaBook className='icon' />
+                    <img src={Haltere} alt="haltere icon" className="icon" />
                 </NavLink>
 
                 <NavLink
                     to={`${basePath}/competition`}
                     className={({ isActive }) => isActive ? "navButton active" : "navButton"}
                 >
-                    <FaTrophy className='icon' />
+                    <img src={Trophy} alt="trophy icon" className="icon" />
                 </NavLink>
 
                 <NavLink
                     to={`${basePath}/profile`}
                     className={({ isActive }) => isActive ? "navButton active" : "navButton"}
                 >
-                    <FaUsers className='icon' />
+                    <img src={Genie} alt="genie icon" className="icon" />
                 </NavLink>
 
                 <NavLink
                     to={`${basePath}/quests`}
                     className={({ isActive }) => isActive ? "navButton active" : "navButton"}
                 >
-                    <FaParachuteBox className='icon' />
+                    <img src={Chest} alt="chest icon" className="icon" />
                 </NavLink>
-
-
-                {/* <button className={`navButton ${activeItem === 'home' ? 'active' : ''}`} onClick={() => handleNavClick('home')}>
-                    <FaHome className='icon' />
-                </button>
-                <button className={`navButton ${activeItem === 'training' ? 'active' : ''}`} onClick={() => handleNavClick('training')}>
-                    <FaBook className='icon' />
-                </button>
-                <button className={`navButton ${activeItem === 'competition' ? 'active' : ''}`} onClick={() => handleNavClick('competition')}>
-                    <FaTrophy className='icon' />
-                </button>
-                <button className={`navButton ${activeItem === 'profile' ? 'active' : ''}`} onClick={() => handleNavClick('profile')}>
-                    <FaUsers className='icon' />
-                </button>
-                <button className={`navButton ${activeItem === 'quests' ? 'active' : ''}`} onClick={() => handleNavClick('quests')}>
-                    <FaParachuteBox className='icon' />
-                </button> */}
             </nav>
         </div>
     );
