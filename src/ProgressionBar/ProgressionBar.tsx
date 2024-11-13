4c355dc2613ed9b8fae290db6b60d3fcb76f6435
Enzo <enzo.pfeiffer@epitech.eu>
2024-11-13 17:44:33 +0100

    fix(reactive css): reactive css for mobile user

---
+++ src/ProgressionBar/ProgressionBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ProgressionBar.css";
+
+interface ProgressBarProps {
+  currentStep: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ currentStep }) => {
+  const progressPercentage = (currentStep / 20) * 100;
+
+  return (
+    <div className="progress-bar-container">
+      <div
+        className="progress-bar-fill"
+        style={{ width: `${progressPercentage}%` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
