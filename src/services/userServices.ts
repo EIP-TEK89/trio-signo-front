a89647de8eaea6bb79dca97ce115d6e75c44f0c9
P4ST4S <antoine.rsopars@epitech.eu>
2024-12-12 12:53:38 +0100

    refactor(UserServices): update comments for user service functions to English

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,27 +1,27 @@
 import { get, post, put, del } from './apiClient';
 import API_ROUTES from './apiRoutes';
 
-// Appel pour récupérer tous les utilisateurs
+// Call to get all users
 export const fetchAllUsers = async () => {
   return await get(API_ROUTES.getAllUsers);
 };
 
-// Appel pour récupérer un utilisateur par ID
+// Call to get a user by email
 export const fetchUserById = async (id: string) => {
   return await get(API_ROUTES.getUserById(id));
 };
 
-// Appel pour créer un utilisateur
+// Call to post a user
 export const signUpUser = async (userData: any) => {
   return await post(API_ROUTES.signUp, userData);
 };
 
-// Appel pour mettre à jour un utilisateur
+// Call to update a user
 export const updateUser = async (id: string, data: any) => {
   return await put(API_ROUTES.updateUser(id), data);
 };
 
-// Appel pour supprimer un utilisateur
+// Call to delete a user
 export const deleteUser = async (id: string) => {
   return await del(API_ROUTES.deleteUser(id));
 };
